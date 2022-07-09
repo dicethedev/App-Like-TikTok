07bfec31b4a06d87a5224646c512a6a05dd850e9
dicetheDev <samuelblessing125@gmail.com>
2022-07-09 11:48:40 +0100

    Sidebar is fixed and working

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,73 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import GoogleLogin from 'react-google-login'
+import { AiFillHome } from 'react-icons/ai'
+import { BiMenu } from 'react-icons/bi'
+import { MdCancel } from 'react-icons/md'
+
+import Discover from './Discover'
+import SuggestedAccounts from './SuggestedAccounts'
+import Footer from './Footer'
+
 
 const Sidebar = () => {
+     const [showSidebar, setShowSidebar] = useState(true);
+
+     const userProfile = false;
+     
+     const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-center cursor-pointer font-semibold text-[#F51997] rounded';
+
      return (
-       <div>Sidebar</div>
+       <div>
+          {/* onClick={() => setShowSidebar(showSidebar => !showSidebar)}> */}
+          <div className="block xl:hidden m-2 ml-4 mt-3 text-xl" 
+           onClick={() => setShowSidebar((prev) => !prev )}>
+            {showSidebar ? <MdCancel /> : <BiMenu /> }
+          </div>
+          {showSidebar && (
+               <div className="xl:w-400 w-20 flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+                <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+                  <Link href="/">
+                    <div className={normalLink}>
+                      <p className="text-2xl"><AiFillHome /></p>
+                       <span className="text-xl hidden xl:block">For you</span>
+                    </div>
+                  </Link>
+                </div>
+
+                    {!userProfile && (
+                    <div className="px-2 py-4 hidden xl:block">
+                         <p className="text-gray-500">Log in to like and comment on videos</p>
+                         <div className="pr-4">
+                              <GoogleLogin
+                                clientId=""
+                                render={(renderProps) => (
+                                   <button
+                                   className="bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] cursor-pointer
+                                   font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]" 
+                                    onClick={renderProps.onClick}
+                                    disabled={renderProps.disabled}
+                                    >
+                                      Log in  
+                                   </button>
+                                )}
+                                onSuccess={() => {}}
+                                onFailure={() => {}}
+                                cookiePolicy='single_host_origin'
+                              />
+                         </div>
+                    </div>
+               )}
+
+               <Discover />
+               <SuggestedAccounts />
+               <Footer />
+               </div>
+          )}
+       </div>
      )
 }
 
