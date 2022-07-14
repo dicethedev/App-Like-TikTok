3fc83d6c2781861f16ce480b9e960a6b1cba93e8
dicetheDev <samuelblessing125@gmail.com>
2022-07-14 01:18:21 +0100

    Video details is working, like button is working & changes apply to some components

---
+++ components/Comments.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { GoVerified } from 'react-icons/go'
+
+import useAuthStore from '../store/authStore'
+import NoResults from '../components/NoResults'
+
+const Comments = () => {
+     return (
+          <div>
+               comments
+          </div>
+     )
+}
+
+export default Comments
--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
-// export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const createOrGetUser = async (response: any, addUser: any) => {
   const decoded: { name: string, picture: string, sub: string } = jwt_decode(response.credential);
@@ -17,7 +18,7 @@ export const createOrGetUser = async (response: any, addUser: any) => {
   addUser(user);
 
   // console.log(decoded)
-  await axios.post(`http://localhost:3000/api/auth`, user)
+  await axios.post(`${BASE_URL}/api/auth`, user)
 };
 
 //  var base64Url = response.credential.split('.')[1];
