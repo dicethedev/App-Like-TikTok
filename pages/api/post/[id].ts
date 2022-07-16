04adcb624ecd5fff2e866f2739d562e9f32be804
dicetheDev <samuelblessing125@gmail.com>
2022-07-16 04:30:13 +0100

    Detail comment section is working and fixed

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+//uuid stand for unique identifier
+import { uuid } from 'uuidv4';
 import { postDetailQuery } from '../../../utils/queries'
 import { client } from '../../../utils/client'
 
@@ -18,5 +19,22 @@ export default async function handler( req: NextApiRequest,
     const data = await client.fetch(query);
 
     res.status(200).json(data[0]);
+  } else if (req.method === 'PUT') {
+    const { comment, userId } = req.body;
+    const { id }:any = req.query;
+
+     const data =  await client
+        .patch(id)
+        .setIfMissing({ comments: [] })
+        .insert('after', 'comments[-1]', [
+             {
+              comment,
+               _key: uuid(),
+               postedBy: { _type: 'postedBy', _ref: userId }
+             }
+        ])
+        .commit()
+
+        res.status(200).json(data);
   }
 }
