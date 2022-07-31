48e0de37d44a8d2a8833cfac020c28deff687857
dicetheDev <samuelblessing125@gmail.com>
2022-07-31 03:50:50 +0100

    change next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //@dev - this will help you to deploy this project without typescript error on deployment build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
